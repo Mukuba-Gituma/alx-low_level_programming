b1bfae3ecff272f6912f4cc2930e07ace8e22280
Mukuba-Gituma <gitumamoureen9@gmail.com>
2022-07-11 02:36:08 -0700

    A function that prints numbers 0 - 9 excluding 2 and 4.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 - 9 with a new line
+ * Description: prints the numbers excluding 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+
+	_putchar('\n');
+}
