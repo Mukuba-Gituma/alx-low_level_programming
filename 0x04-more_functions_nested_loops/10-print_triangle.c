262ea7f2f2307bc13a1a45b71f07a899b728e9ee
Mukuba-Gituma <gitumamoureen9@gmail.com>
2022-07-13 05:00:38 -0700

    A function that prints a triangle of size SIZE.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of size SIZE
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int x, y, space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x - 1; x <= size; x++)
+		{
+			for (space = 1; space <= (space - x); space++)
+			{
+				_putchar(' ');
+			}
+			for (y = 1; y <= x; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
