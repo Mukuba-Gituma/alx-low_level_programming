2757d066f0b79fd4dc72a58611ff06bfafcaa2cc
Mukuba-Gituma <gitumamoureen9@gmail.com>
2022-07-19 06:44:55 -0700

    Function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int byteCount;
 
-	for (byteCount = 0; byteCount < n && src[byteCount] != '\0', byteCount++)
+	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
 	{
 		dest[byteCount] = src[byteCount];
 
